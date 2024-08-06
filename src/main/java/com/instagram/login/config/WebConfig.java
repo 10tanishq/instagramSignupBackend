ada863074926f426a9275c8e935ae2f854d4a744
Tanishq <tanishqmlzs786@gmail.com>
2024-08-06 22:51:05 +0530

    adding vercel frontend url

--- src/main/java/com/instagram/login/config/WebConfig.java
+++ src/main/java/com/instagram/login/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // Change to your frontend server URL
+                .allowedOrigins("http://localhost:3000", "https://insta-signup.vercel.app") // Allow both localhost and Vercel frontend URLs
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 }
